e3f92d8877c64aaa124141a4ba9750d1e57fc99d
Lalita Dulma <lalita.du@sansiri.com>
2023-01-28 21:19:36 +0700

    create login,signup page

---
+++ src/configs/common.ts
@@ -0,0 +1,15 @@
+import {CompositeNavigationProp} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {BottomTabNavigationProp} from '@react-navigation/bottom-tabs';
+
+export type ScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabParamList>,
+  NativeStackNavigationProp<RootStackParamList>
+>;
+
+export type TabParamList = {};
+
+export type RootStackParamList = {
+  LOGIN: {};
+  SIGNUP: {};
+};
