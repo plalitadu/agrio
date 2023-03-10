48f2f5a2c3d05f8b338876213b16a93b21f358b9
thanaphon <>
2023-03-10 14:33:33 +0700

    update

--- src/navigation/MainTab.tsx
+++ src/navigation/MainTab.tsx
@@ -65,7 +65,7 @@ const tabBarOption: BottomTabNavigationOptions = {
     //   fontSize: Scale.point(12),
     // position:'relative',
     // height: 0,
-    fontFamily: 'GraphikTH-SemiBold',
+    // fontFamily: 'GraphikTH-SemiBold',
   },
 };
 
